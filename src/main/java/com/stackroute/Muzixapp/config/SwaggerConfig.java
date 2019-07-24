56a1c63958ab64b6d954b8d5701c6f3687670c69
Moupali Dutta <moupali.dutta.68@gmail.com>
2019-07-24 17:31:36 +0530

    Task5 using Exception completed

---
+++ src/main/java/com/stackroute/Muzixapp/config/SwaggerConfig.java
@@ -0,0 +1,4 @@
+package com.stackroute.Muzixapp.config;
+
+public class SwaggerConfig {
+}
---
+++ src/main/java/com/stackroute/Muzixapp/exception/UserAlreadyExistsException.java
@@ -0,0 +1,4 @@
+package com.stackroute.Muzixapp.exception;
+
+public class UserAlreadyExistsException {
+}
