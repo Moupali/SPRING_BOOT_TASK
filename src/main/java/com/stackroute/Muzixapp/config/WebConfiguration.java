743a47f34ed37219bf8648b6fc37fdf8986af7d8
Moupali Dutta <32608821+Moupali@users.noreply.github.com>
2019-08-06 20:15:03 +0530

    Update WebConfiguration.java

--- src/main/java/com/stackroute/Muzixapp/config/WebConfiguration.java
+++ src/main/java/com/stackroute/Muzixapp/config/WebConfiguration.java
@@ -4,7 +4,7 @@ import org.h2.server.web.WebServlet;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+@EnableSwagger2 //enabling swagger2
 @Configuration
 public class WebConfiguration {
     @Bean
@@ -13,4 +13,22 @@ public class WebConfiguration {
         registrationBean.addUrlMappings("/console/*");
         return registrationBean;
     }
+    
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors
+                        .basePackage("com.stackroute.muzixapplication.controller"))
+                .paths(PathSelectors.regex("/.*"))
+                .build().apiInfo(apiEndPointsInfo());
+    }
+    private ApiInfo apiEndPointsInfo() {
+        return new ApiInfoBuilder().title("Spring Boot REST API")
+                .description("Music Management REST API")
+               // .contact(new Contact("Ramesh gupta", "www.javatpoint.com", "ramesh24gupta@gmail.com"))
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .version("1.0.0")
+                .build();
+   }
 }
