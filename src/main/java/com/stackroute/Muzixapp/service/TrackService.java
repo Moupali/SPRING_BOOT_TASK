b43b32cc484e2c16a06f61ce864b69ac7ac75e18
Moupali Dutta <32608821+Moupali@users.noreply.github.com>
2019-08-07 11:23:23 +0530

    Update TrackService.java

--- src/main/java/com/stackroute/Muzixapp/service/TrackService.java
+++ src/main/java/com/stackroute/Muzixapp/service/TrackService.java
@@ -3,12 +3,12 @@ package com.stackroute.Muzixapp.service;
 import com.stackroute.Muzixapp.domain.Track;
 import com.stackroute.Muzixapp.exception.TrackNotFoundException;
 import com.stackroute.Muzixapp.exception.UserAlreadyExistsException;
-//import com.stackroute.Muzixapp.exceptions.TrackAlreadyExistsException;
+
 
 import java.util.List;
 
 public interface TrackService {
-    public Track saveTrack(Track track); //throws TrackAlreadyExistsException;
+    public Track saveTrack(Track track); 
     public List<Track> getAllTracks();
     public Track getTrackById(int id) throws TrackNotFoundException;
     public void deleteTrack(int id);
